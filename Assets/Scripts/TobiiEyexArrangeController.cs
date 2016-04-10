dd8c5dac56076312d19a3c678e9f16f932ec63cb
tomohage <arys.tmk1104@gmail.com>
2016-04-11 08:26:01 +0900

    初期コミット

---
+++ Assets/Scripts/TobiiEyexArrangeController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class TobiiEyexArrangeController : MonoBehaviour {
+    private Vector2 _eyePosition;
+    void Awake ()
+    {
+    }
+	// Use this for initialization
+	void Start ()
+    {
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        var lastEyePosition = gameObject.GetComponent<EyePositionDataComponent>().LastEyePosition;
+        var lastGazePosition = gameObject.GetComponent<GazePointDataComponent>().LastGazePoint;
+        _eyePosition = lastGazePosition.Display;
+        // 両目が空いているときの挙動
+        if (lastEyePosition.RightEye.IsValid && lastEyePosition.LeftEye.IsValid)
+        {
+            Debug.Log("Both: " + _eyePosition.x + ", " + _eyePosition.y);
+        }
+        // 右目が空いているときの挙動
+        else if (lastEyePosition.RightEye.IsValid)
+        {
+            Debug.Log("Right: " + _eyePosition.x + ", " + _eyePosition.y);
+        }
+        // 左目が空いているときの挙動
+        else if (lastEyePosition.LeftEye.IsValid)
+        {
+            Debug.Log("Left: " + _eyePosition.x + ", " + _eyePosition.y);
+        }
+        else
+        {
+            Debug.Log("None");
+        }
+    }
+}
